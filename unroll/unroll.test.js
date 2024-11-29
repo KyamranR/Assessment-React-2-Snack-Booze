62932b6d3c1366a74fd376561336a9f100f6b94d
Kyamran <kyamranr@gmail.com>
2024-11-29 11:15:26 -0500

    finished unroll exercise and wrote some tests

--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -1,9 +1,37 @@
 const unroll = require("./unroll");
 
 describe("#unroll", function () {
-
   it("is a function", function () {
     expect(typeof unroll).toEqual("function");
   });
 
+  it("should return correct order", function () {
+    const square = [
+      [1, 2, 3, 4],
+      [5, 6, 7, 8],
+      [9, 10, 11, 12],
+      [13, 14, 15, 16],
+    ];
+    expect(unroll(square)).toEqual([
+      1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10,
+    ]);
+  });
+  it("should return correct order for a 3x3 grid", function () {
+    const smallerSquare = [
+      ["a", "b", "c"],
+      ["d", "e", "f"],
+      ["g", "h", "i"],
+    ];
+    expect(unroll(smallerSquare)).toEqual([
+      "a",
+      "b",
+      "c",
+      "f",
+      "i",
+      "h",
+      "g",
+      "d",
+      "e",
+    ]);
+  });
 });
