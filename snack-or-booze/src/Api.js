e78fd6fdf223d8585ca0793b47e6fc50a48c5184
Kyamran <kyamranr@gmail.com>
2024-11-30 16:24:31 -0500

    added getDrinks and addItem functions and changed the base url to 3000

--- snack-or-booze/src/Api.js
+++ snack-or-booze/src/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_API_URL = "http://localhost:5000";
+const BASE_API_URL = "http://localhost:3000";
 
 /* 
   json-server will give you CRUD endpoints on snacks and drinks.
@@ -10,12 +10,20 @@ const BASE_API_URL = "http://localhost:5000";
 */
 
 class SnackOrBoozeApi {
-
   static async getSnacks() {
     const result = await axios.get(`${BASE_API_URL}/snacks`);
     return result.data;
   }
 
+  static async getDrinks() {
+    const result = await axios.get(`${BASE_API_URL}/drinks`);
+    return result.data;
+  }
+
+  static async addItem(type, item) {
+    const result = await axios.post(`${BASE_API_URL}/${type}`, item);
+    return result.data;
+  }
 }
 
 export default SnackOrBoozeApi;
