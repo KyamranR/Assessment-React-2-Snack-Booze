9b19dc7d4258935cbfaf74dd6c7142ebe817ebda
Kyamran <kyamranr@gmail.com>
2024-12-02 18:01:09 -0600

    after submitting form it resets and redirects to the correct route

--- snack-or-booze/src/AddItemForm.js
+++ snack-or-booze/src/AddItemForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { FormGroup } from "reactstrap";
+import { FormGroup, Card, CardBody } from "reactstrap";
+import { useHistory } from "react-router-dom";
 
 function AddItemForm({ addNewItem }) {
   const [formData, setFormData] = useState({
@@ -10,6 +11,8 @@ function AddItemForm({ addNewItem }) {
     serve: "",
   });
 
+  const history = useHistory();
+
   const handleChange = (evt) => {
     const { name, value } = evt.target;
     setFormData((data) => ({ ...data, [name]: value }));
@@ -19,44 +22,65 @@ function AddItemForm({ addNewItem }) {
     evt.preventDefault();
     const { type, ...itemData } = formData;
     addNewItem(type, itemData);
+    setFormData({
+      type: "snacks",
+      name: "",
+      description: "",
+      recipe: "",
+      serve: "",
+    });
+
+    history.push(`/${type}`);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <FormGroup>
-        <label>Type:</label>
-        <select name="type" value={formData.type} onChange={handleChange}>
-          <option value="snacks">Snack</option>
-          <option value="drinks">Drink</option>
-        </select>
-      </FormGroup>
-      <FormGroup>
-        <label>Name:</label>
-        <input
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-      </FormGroup>
-      <FormGroup>
-        <label>Description:</label>
-        <input
-          name="description"
-          value={formData.description}
-          onChange={handleChange}
-        />
-      </FormGroup>
-      <FormGroup>
-        <label>Recipe:</label>
-        <input name="recipe" value={formData.recipe} onChange={handleChange} />
-      </FormGroup>
-      <FormGroup>
-        <label>Serve:</label>
-        <input name="serve" value={formData.serve} onChange={handleChange} />
-      </FormGroup>
-      <button>Add Item</button>
-    </form>
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <CardBody>
+          <FormGroup>
+            <label>Type:</label>
+            <select name="type" value={formData.type} onChange={handleChange}>
+              <option value="snacks">Snack</option>
+              <option value="drinks">Drink</option>
+            </select>
+          </FormGroup>
+          <FormGroup>
+            <label>Name:</label>
+            <input
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </FormGroup>
+          <FormGroup>
+            <label>Description:</label>
+            <input
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <label>Recipe:</label>
+            <input
+              name="recipe"
+              value={formData.recipe}
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+            <label>Serve:</label>
+            <input
+              name="serve"
+              value={formData.serve}
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <button>Add Item</button>
+        </CardBody>
+      </form>
+    </Card>
   );
 }
 
